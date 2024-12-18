0530a19a59966db6734f3644e53d6385b6b21fb2
danielmbuthia <danielwandurua@gmail.com>
2024-12-18 23:00:15 +0300

    setup

---
+++ news-hub-backend/routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::post('/login', [AuthController::class, 'login'])->name('v1.login');
+    Route::post('/register', [AuthController::class, 'register'])->name('v1.register');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout'])->name('v1.logout');
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        })->name('v1.user');
+    });
+});
